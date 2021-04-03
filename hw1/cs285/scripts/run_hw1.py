22a6bd19a15dc90e1285245ce3d5d76850fe2ae5
Enmei Tu <hellotem@hotmail.com>
2021-04-03 12:31:54 +0800

    change default GPU No

--- hw1/cs285/scripts/run_hw1.py
+++ hw1/cs285/scripts/run_hw1.py
@@ -76,7 +76,7 @@ def main():
     parser.add_argument('--video_log_freq', type=int, default=5)
     parser.add_argument('--scalar_log_freq', type=int, default=1)
     parser.add_argument('--no_gpu', '-ngpu', action='store_true')
-    parser.add_argument('--which_gpu', type=int, default=0)
+    parser.add_argument('--which_gpu', type=int, default=1)
     parser.add_argument('--max_replay_buffer_size', type=int, default=1000000)
     parser.add_argument('--save_params', action='store_true')
     parser.add_argument('--seed', type=int, default=1)
@@ -84,7 +84,7 @@ def main():
 
     # convert args to dictionary
     params = vars(args)
-
+#test
     ##################################
     ### CREATE DIRECTORY FOR LOGGING
     ##################################
